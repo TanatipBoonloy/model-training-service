9dc0e190266b30ce92b912d675ff942f06cd05fb
Tanatip Boonloy <Tanatip Boonloy>
2017-04-03 10:47:17 +0700

    init project

---
+++ src/main/java/kmitl/sp/smp/ModelTrainingServiceApplication.java
@@ -0,0 +1,12 @@
+package kmitl.sp.smp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelTrainingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelTrainingServiceApplication.class, args);
+	}
+}
