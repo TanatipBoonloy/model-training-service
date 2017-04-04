ebbb8a001820a4b0c96688565801aa8c930972ce
Tanatip Boonloy <Tanatip Boonloy>
2017-04-04 07:46:48 +0700

    config cors & fix bugs

--- src/main/java/kmitl/sp/smp/controller/Controller.java
+++ src/main/java/kmitl/sp/smp/controller/Controller.java
@@ -3,16 +3,14 @@ package kmitl.sp.smp.controller;
 import kmitl.sp.smp.model.server.response.base.ApiBaseResponse;
 import kmitl.sp.smp.service.ControllerService;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
 
 /**
  * Created by Jo on 4/3/2017.
  */
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping(value = "api/v1")
 public class Controller {
--- src/main/java/kmitl/sp/smp/entity/SuggestedMusic.java
+++ src/main/java/kmitl/sp/smp/entity/SuggestedMusic.java
@@ -2,10 +2,7 @@ package kmitl.sp.smp.entity;
 
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Date;
 
 /**
@@ -17,6 +14,7 @@ import java.util.Date;
 public class SuggestedMusic {
     @Id
     @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
     @Column(name = "user_id")
